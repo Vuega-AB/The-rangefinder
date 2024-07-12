8be823063f12115b566a03303b942ae3794775c4
Nisreen Hisham <157918418+nhi-vuega@users.noreply.github.com>
2024-07-12 15:26:56 +0300

    for publishing

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('app.html', api_key=os.getenv('GOOGLE_MAPS_API_KEY'))
+    api_key = os.getenv('GOOGLE_MAPS_API_KEY')
+    return render_template('app.html', api_key=api_key)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
